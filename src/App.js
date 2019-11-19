0964f63e0183b1f6494647406a48f35cf60fbd04
King Elisha <ek.chibueze@gmail.com>
2019-11-19 14:18:59 +0100

    home route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.scss';
+import Home from './components/Home/Home';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <Route path="/" component={Home} />
+    </Router>
   );
 }
 
