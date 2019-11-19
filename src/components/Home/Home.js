a3bf05a5123759e77bfabaf20d6734d7da11b66e
King Elisha <ek.chibueze@gmail.com>
2019-11-19 14:32:35 +0100

    navbar

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import './Home.scss';
 
+import Navbar from '../layout/Navbar/Navbar';
+
 export default class Home extends Component {
   render() {
     return (
       <div className="home">
+        <Navbar />
         <h1>Hello</h1>
       </div>
     )
